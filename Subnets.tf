a88cfb3e1bd0956d6a07b507bc8008b2cbdd6b9d
Sen <140600361+sen-23-code@users.noreply.github.com>
2024-08-30 17:55:26 +0530

    Update Subnets.tf

--- Subnets.tf
+++ Subnets.tf
@@ -14,6 +14,6 @@ resource "aws_subnet" "public-2" {
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name = "public-sub-2"
+    Name = "public-sub-3"
   }
 }
